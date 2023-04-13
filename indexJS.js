49bb4ce71f8f210a53f605621e563d17fdcd31ad
Ge0rgeM <99915251+Ge0rgeM@users.noreply.github.com>
2023-04-13 16:46:40 +0400

    Add files via upload

--- indexJS.js
+++ indexJS.js
@@ -6,12 +6,34 @@ const resize_ob = new ResizeObserver(function(entries) {
     let width = rect.width;
     let height = rect.height;
 
-    console.log('Current Width : ' + width);
-    console.log('Current Height : ' + height);
+    // console.log('Current Width : ' + width);
+    // console.log('Current Height : ' + height);
     //write font code to change accordingly
     let divFont = document.getElementsByClassName("buttons")[0].children;
     for(let i=0;i<divFont.length;i++)
         divFont[i].style.fontSize = (width/4*10)/100+"px";
-    console.log(divFont);
+    // console.log(divFont);
 });
 resize_ob.observe(document.getElementsByClassName("buttons")[0]);
+
+projectClick();
+function projectClick() {
+    let projects = document.getElementById("portfolioDivs").children;
+    for(let i=0;i<projects.length;i++) {
+        projects[i].addEventListener("click",function() {
+            const newTab = window.open(projectLinks[projects[i].id],"_blank");
+        });
+        projects[i].addEventListener('mousedown',function(e) {
+            if(e.button==1){
+                window.open(projectLinks[projects[i].id]);
+            }
+        });
+    }
+}
+//Project Links Obj
+const projectLinks={};
+projectLinks["TicTacToe"]="https://ge0rgem.github.io/TicTacToe/";
+projectLinks["ToDoList"]="https://ge0rgem.github.io/ToDoList/";
+projectLinks["StopWatch"]="https://ge0rgem.github.io/Stopwatch/";
+projectLinks["ComingSoon"]="#";
+console.log(projectLinks);
