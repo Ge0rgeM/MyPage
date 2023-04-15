f815ecd67a5dcf83441d8e0d166ff2ad6c4d14c5
Ge0rgeM <99915251+Ge0rgeM@users.noreply.github.com>
2023-04-15 20:03:40 +0400

    Add files via upload

--- indexJS.js
+++ indexJS.js
@@ -16,6 +16,60 @@ const resize_ob = new ResizeObserver(function(entries) {
 });
 resize_ob.observe(document.getElementsByClassName("buttons")[0]);
 
+const target = [];
+target.push(document.getElementsByClassName("frontTextIntro")[0]);
+// target.push(document.getElementById("portfolioDivs"));
+target.push(document.getElementsByClassName("portfolioDiv")[0].children[0]);
+target.push(...document.getElementsByClassName("portfolioDiv")[0].children[1].children);
+console.log(target);
+// const items = document.querySelectorAll('.item');
+
+function checkViewport() {
+    target.forEach(item => {
+        const itemPosition = item.getBoundingClientRect();
+        // Check if the item is in the viewport
+        if (itemPosition.top < window.innerHeight-window.innerHeight*12/100 
+            && itemPosition.bottom >= window.innerHeight*22/100) {
+                console.log(item,itemPosition.top,itemPosition.bottom);
+                item.style.opacity = 1;
+        } else {
+            item.style.opacity = 0;
+        }
+    });
+}
+window.addEventListener('load', checkViewport);
+window.addEventListener('scroll', checkViewport);
+
+
+/* target.forEach(el => {
+    el.classList.remove("div-transition");
+});
+function callback(entries, observer) {
+    for(let i=0;i<entries.length;i++){
+        if(entries[i].isIntersecting) {
+            console.log("visible");
+            target[i].classList.add("div-transition");
+        } else {
+            console.log("not visible");
+            target[i].classList.remove("div-transition");
+            // target.classList.remove("div-transition"); 
+       }
+    }
+}
+function createObserver(target, callback) {
+   const options = {
+      root: null,
+      threshold: 0.4
+   };
+   const observer = new IntersectionObserver(callback, options);
+   target.forEach(el => {
+        observer.observe(el);
+    });
+}
+createObserver(target, callback); */
+
+
+
 projectClick();
 function projectClick() {
     let projects = document.getElementById("portfolioDivs").children;
