381c54d53c2cf8318c3ae1217750007c5b9f34ec
Ge0rgeM <99915251+Ge0rgeM@users.noreply.github.com>
2023-04-16 07:08:54 +0400

    Add files via upload
    Do something about frontextdiv

--- indexJS.js
+++ indexJS.js
@@ -1,4 +1,6 @@
-const resize_ob = new ResizeObserver(function(entries) {
+projectClick();
+
+const resize_navButtons = new ResizeObserver(function(entries) {
     // since we are observing only a single element, so we access the first element in entries array
     let rect = entries[0].contentRect;
 
@@ -14,14 +16,30 @@ const resize_ob = new ResizeObserver(function(entries) {
         divFont[i].style.fontSize = (width/4*10)/100+"px";
     // console.log(divFont);
 });
-resize_ob.observe(document.getElementsByClassName("buttons")[0]);
+resize_navButtons.observe(document.getElementsByClassName("buttons")[0]);
+const resize_ob = new ResizeObserver(function(entries) {
+    // since we are observing only a single element, so we access the first element in entries array
+    let rect = entries[0].contentRect;
+    // current width & height
+    let width = rect.width;
+    let height = rect.height;
+    
+    // console.log('Current Width : ' + width);
+    // console.log('Current Height : ' + height);
+    //write font code to change accordingly
+    let divToResize = document.getElementsByClassName("frontTextIntro")[0];
+    console.log(height);
+    divToResize.style.paddingTop = 4.5*height + "px";
+    // console.log(divFont);
+});
+resize_ob.observe(document.getElementsByClassName("navDiv")[0]);
 
 const target = [];
 target.push(document.getElementsByClassName("frontTextIntro")[0]);
 // target.push(document.getElementById("portfolioDivs"));
 target.push(document.getElementsByClassName("portfolioDiv")[0].children[0]);
 target.push(...document.getElementsByClassName("portfolioDiv")[0].children[1].children);
-console.log(target);
+target.push(document.querySelectorAll(".formDiv>form")[0]);
 // const items = document.querySelectorAll('.item');
 
 function checkViewport() {
@@ -30,7 +48,6 @@ function checkViewport() {
         // Check if the item is in the viewport
         if (itemPosition.top < window.innerHeight-window.innerHeight*12/100 
             && itemPosition.bottom >= window.innerHeight*22/100) {
-                console.log(item,itemPosition.top,itemPosition.bottom);
                 item.style.opacity = 1;
         } else {
             item.style.opacity = 0;
@@ -70,7 +87,12 @@ createObserver(target, callback); */
 
 
 
-projectClick();
+//Project Links Obj
+const projectLinks={};
+projectLinks["TicTacToe"]="https://ge0rgem.github.io/TicTacToe/";
+projectLinks["ToDoList"]="https://ge0rgem.github.io/ToDoList/";
+projectLinks["StopWatch"]="https://ge0rgem.github.io/Stopwatch/";
+projectLinks["ComingSoon"]="#";
 function projectClick() {
     let projects = document.getElementById("portfolioDivs").children;
     for(let i=0;i<projects.length;i++) {
@@ -84,10 +106,3 @@ function projectClick() {
         });
     }
 }
-//Project Links Obj
-const projectLinks={};
-projectLinks["TicTacToe"]="https://ge0rgem.github.io/TicTacToe/";
-projectLinks["ToDoList"]="https://ge0rgem.github.io/ToDoList/";
-projectLinks["StopWatch"]="https://ge0rgem.github.io/Stopwatch/";
-projectLinks["ComingSoon"]="#";
-console.log(projectLinks);
